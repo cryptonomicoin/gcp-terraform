0d3b194ba7caa6d6e8ab43c077b8283f526bf973
Matthew Brown <matt@Matts-CyCode-Macbook-Pro.local>
2022-01-25 14:11:03 -0800

    add vars

--- main.tf
+++ main.tf
@@ -26,9 +26,3 @@ resource "google_compute_network" "vpc_network" {
   name                    = "terraform-network"
   auto_create_subnetworks = "true"
 }
-
-resource "google_storage_bucket" "open-bucket" {
-  name          = "open-bucket"
-  location      = var.region
-  project       = var.project
-}
