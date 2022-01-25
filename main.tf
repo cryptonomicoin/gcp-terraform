5115dfdac1134d21221ceb9b327d3df5f37c262b
Matthew Brown <matt@Matts-CyCode-Macbook-Pro.local>
2022-01-24 17:00:39 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "flask-app-platform"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
