09aab001bd700757a2f780b187cf813f132a877f
sf-matt <87498061+sf-matt@users.noreply.github.com>
2022-03-02 10:40:37 -0800

    Update storage_bucket.tf

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -4,6 +4,12 @@ resource "google_storage_bucket" "open-bucket" {
   project       = var.project
 }
 
+resource "google_storage_bucket" "public-bucket" {
+  name          = "open-bucket"
+  location      = var.region
+  project       = var.project
+}
+
 
 resource "google_storage_bucket_iam_binding" "binding" {
   bucket = google_storage_bucket.open-bucket.name
@@ -20,4 +26,10 @@ resource "google_storage_bucket_acl" "open-bucket-acl" {
   role_entity = [
     "READER:allUsers",
   ]
+}
+
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.public-bucket.name
+  role   = "READER"
+  entity = "allUsers"
 }
