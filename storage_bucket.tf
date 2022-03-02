451d45ddad8494d5d88c254ef0741f44e1600a80
sf-matt <87498061+sf-matt@users.noreply.github.com>
2022-03-02 09:41:14 -0800

    Update storage_bucket.tf

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -3,3 +3,9 @@ resource "google_storage_bucket" "open-bucket" {
   location      = var.region
   project       = var.project
 }
+
+resource "google_storage_bucket_acl" "open-bucket-acl" {
+  bucket = google_storage_bucket.open-bucket.name
+
+  predefined_acle = publicReadWrite
+}
