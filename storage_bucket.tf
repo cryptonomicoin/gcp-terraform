c3307c5a9c3ead3a2ba4cea487480b179a24e3ac
gnair <girish.nair@live.com>
2022-03-10 09:15:25 -0600

    [Cycode] Fix for IaC misconfiguration - Ensure that Cloud Storage buckets have uniform bucket-level access enabled

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "really-open-bucket" {
   name          = "really-open-bucket"
   location      = var.region
   project       = var.project
+  uniform_bucket_level_access = true
 }
 
 resource "google_storage_bucket" "public-bucket" {
