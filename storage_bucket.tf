9088c10fe905e3e87ec90a97a94427acf45cd097
Matthew Brown <matt@Matts-CyCode-Macbook-Pro.local>
2022-03-16 10:21:45 -0700

    trimmed buckets

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -5,39 +5,9 @@ resource "google_storage_bucket" "really-open-bucket" {
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket" "public-bucket" {
-  name          = "public-bucket"
+resource "google_storage_bucket" "public-bucket-cycse" {
+  name          = "public-bucket-cycse"
   location      = var.region
   project       = var.project
   uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket" "not-another-public-bucket" {
-  name          = "public-bucket"
-  location      = var.region
-  project       = var.project
-  uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_iam_binding" "binding" {
-  bucket = google_storage_bucket.really-open-bucket.name
-  role   = "roles/storage.objectViewer"
-
-  members = [
-    "allUsers",
-  ]
-}
-
-resource "google_storage_bucket_acl" "open-bucket-acl" {
-  bucket = google_storage_bucket.really-open-bucket.name
-
-  role_entity = [
-    "READER:allUsers",
-  ]
-}
-
-resource "google_storage_default_object_access_control" "public_rule" {
-  bucket = google_storage_bucket.public-bucket.name
-  role   = "READER"
-  entity = "allUsers"
 }
